7968c72b73469581c7f16402ec9b1d99bce98836
IWasYourTomb <ganin@factory.codes>
2023-12-24 01:36:03 +0300

    Initial Commit

---
+++ barcodeScanerApp/barcodeScanerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeScanerAppApp.swift
+//  barcodeScanerApp
+//
+//  Created by matvey on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct barcodeScanerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
